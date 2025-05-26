7b8d7a1a1b7b10315cda94debbe0eaa5c4d3eb39
Muhammad Rizwan <muhammad.rizwan@videoslots.com>
2025-05-27 01:04:14 +0200

    Improve facade accessor api . updated readme.

--- src/Facades/FCGI.php
+++ src/Facades/FCGI.php
@@ -6,17 +6,39 @@ use Illuminate\Support\Facades\Facade;
 use Rizwan\LaravelFcgiClient\FCGIManager;
 
 /**
- * Laravel Facade for FCGIManager.
+ * Laravel Facade for FCGIManager - Complete Laravel HTTP Client API for FastCGI.
  *
- * Provides static access to the FastCGI client manager methods.
+ * Provides a Laravel HTTP client-like interface for communicating with FastCGI servers,
+ * with full support for URL templates, content types, authentication, and retry logic.
  *
- * @method static \Rizwan\LaravelFcgiClient\Responses\Response get(string $host, string $scriptPath, array $options = [], ?int $port = null)
- * @method static \Rizwan\LaravelFcgiClient\Responses\Response post(string $host, string $scriptPath, array $options = [], ?int $port = null)
- * @method static \Rizwan\LaravelFcgiClient\Responses\Response put(string $host, string $scriptPath, array $options = [], ?int $port = null)
- * @method static \Rizwan\LaravelFcgiClient\Responses\Response patch(string $host, string $scriptPath, array $options = [], ?int $port = null)
- * @method static \Rizwan\LaravelFcgiClient\Responses\Response delete(string $host, string $scriptPath, array $options = [], ?int $port = null)
- * @method static \Rizwan\LaravelFcgiClient\Responses\Response json(string $host, string $scriptPath, array $options = [], ?int $port = null)
-
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withHeaders(array $headers)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withHeader(string $name, string $value)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withBody(string $content, string $contentType = 'text/plain')
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withToken(string $token, string $type = 'Bearer')
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withBasicAuth(string $username, string $password)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withUserAgent(string $userAgent)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withUri(string $uri)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withUrlParameters(array $parameters)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withServerParams(array $params)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withCustomVars(array $vars)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withQuery(array $query)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager withPayload(array $data)
+ *
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager asJson()
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager asForm()
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager acceptJson()
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager accept(string $contentType)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager contentType(string $contentType)
+ *
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager timeout(int $seconds)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager connectTimeout(int $seconds)
+ * @method static \Rizwan\LaravelFcgiClient\FCGIManager retry(int $times, int $sleepMilliseconds = 0, ?callable $when = null)
+ *
+ * @method static \Rizwan\LaravelFcgiClient\Responses\Response get(string $host, string $scriptPath, array $query = [], ?int $port = null)
+ * @method static \Rizwan\LaravelFcgiClient\Responses\Response post(string $host, string $scriptPath, array $data = [], ?int $port = null)
+ * @method static \Rizwan\LaravelFcgiClient\Responses\Response put(string $host, string $scriptPath, array $data = [], ?int $port = null)
+ * @method static \Rizwan\LaravelFcgiClient\Responses\Response patch(string $host, string $scriptPath, array $data = [], ?int $port = null)
+ * @method static \Rizwan\LaravelFcgiClient\Responses\Response delete(string $host, string $scriptPath, array $data = [], ?int $port = null)
  */
 final class FCGI extends Facade
 {
